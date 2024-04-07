e435bf1326dbbfa19a7d752bb1061f321c2fcfcc
Coder Nazmul Hossain <nazmulhossain3691215@gmail.com>
2024-04-08 02:00:22 +0600

    added UserController

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\HomeController;
 Route::get('/', function () {
     return view('welcome');
 });
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::get('/profile', [UserController::class, 'profile'])->name('profile');
