a6f58f6f7d76e3e9f24f6a6484e1d4787efced7d
Coder Nazmul Hossain <nazmulhossain3691215@gmail.com>
2024-04-08 23:27:15 +0600

    email blade added

--- app/Mail/SingleOffer.php
+++ app/Mail/SingleOffer.php
@@ -37,7 +37,7 @@ class SingleOffer extends Mailable
     public function content(): Content
     {
         return new Content(
-            view: 'view.name',
+            view: 'email.email',
         );
     }
 
